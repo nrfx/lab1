9de8c5e278740ad7118ceb4ae847a7aae455a952
nrfx <57629383+nrfx@users.noreply.github.com>
2024-10-12 20:59:40 +0800

    2nd code

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x, y;
+	cout << "введите координаты точки" << endl;
+	cin >> x >> y;
+	if ((x > -1) && (x < 1) && (y > -1) && (y < 1))
+	{
+		cout << "точка принадлежит заштрихованной области (1;1)" << endl;
+	}
+	else cout << "точка не принадлежит заштрихованной области (1;1)" << endl;
+	return 0;
+}
